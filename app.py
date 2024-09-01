61b0e40c87ce149802230760e6b230b90c237bf0
uliang1024 <amisleo000@gmail.com>
2024-09-01 18:58:37 +0800

    update app

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     req = request.get_json()
-    print(req)
     reText = req["queryResult"]["fulfillmentText"]  # 取得 Dialogflow 的回應文字
     intent = req["queryResult"]["intent"]["displayName"]  # 取得 intent 分類
     replytoken = req["originalDetectIntentRequest"]["payload"]["data"][
@@ -23,25 +22,26 @@ def webhook():
     # 雷達回波圖網址，後方加上時間戳記，避免緩存
     img = f"https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-003.png?{time.time_ns()}"
     # 如果收到的 intent 是 radar
-    if intent == "radar":
+    if intent == "radar(氣象)":
         headers = {
             "Authorization": "Bearer " + token,
             "Content-Type": "application/json",
         }
+        print(token)
         body = {
             "replyToken": replytoken,
             "messages": [
                 {"type": "image", "originalContentUrl": img, "previewImageUrl": img}
             ],
         }
-        # 使用 requests 方法回傳訊息到 ＬINE
+        # 使用 requests 方法回傳訊息到 Line
         result = requests.request(
             "POST",
             "https://api.line.me/v2/bot/message/reply",
             headers=headers,
             data=json.dumps(body).encode("utf-8"),
         )
-        print(result.text)
+        # print(result.text)
         # 完成後回傳訊息到 Dialogflow
         return {"source": "webhookdata"}
     # 如果收到的 intent 不是 radar
